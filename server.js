4a0364a1c45a08c0ca3da32e6b60cdde2b1b1be1
w.hunt <wfh2d88@gmail.com>
2021-02-25 12:22:22 -0600

    11.3.8

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 const { animals } = require("./data/animals");
 const fs = require('fs');
 const path = require('path');
@@ -116,6 +117,19 @@ app.post('/api/animals', (req, res) => {
     res.json(animal);
   }
 });
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}`);
