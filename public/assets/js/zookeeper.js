2151cf31ee738de82f1f2ec4ef880651774890d9
w.hunt <wfh2d88@gmail.com>
2021-03-01 10:23:38 -0600

    done

--- public/assets/js/zookeeper.js
+++ public/assets/js/zookeeper.js
@@ -1,23 +1,24 @@
+const $zookeeperForm = document.querySelector('#zookeeper-form');
 const $displayArea = document.querySelector('#display-area');
 
 const printResults = resultArr => {
   console.log(resultArr);
 
-  const animalHTML = resultArr.map(({ id, name, age, favoriteAnimal }) => {
+  const zookeeperHTML = resultArr.map(({ id, name, age, favoriteAnimal }) => {
     return `
   <div class="col-12 col-md-5 mb-3">
     <div class="card p-3" data-id=${id}>
       <h4 class="text-primary">${name}</h4>
       <p>Age: ${age}<br/>
       Favorite Animal: ${favoriteAnimal.substring(0, 1).toUpperCase() +
-        favoriteAnimal.substring(1)}<br/>
+      favoriteAnimal.substring(1)}<br/>
       </p>
     </div>
   </div>
     `;
   });
 
-  $displayArea.innerHTML = animalHTML.join('');
+  $displayArea.innerHTML = zookeeperHTML.join('');
 };
 
 const getZookeepers = (formData = {}) => {
@@ -39,5 +40,20 @@ const getZookeepers = (formData = {}) => {
       printResults(zookeeperArr);
     });
 };
+
+const handleGetZookeepersSubmit = event => {
+  event.preventDefault();
+  const nameHTML = $zookeeperForm.querySelector('[name="name"]');
+  const name = nameHTML.value;
+
+  const ageHTML = $zookeeperForm.querySelector('[name="age"]');
+  const age = ageHTML.value;
+
+  const zookeeperObject = { name, age };
+
+  getZookeepers(zookeeperObject);
+};
+
+$zookeeperForm.addEventListener('submit', handleGetZookeepersSubmit);
 
 getZookeepers();
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -2,15 +2,15 @@ const path = require('path');
 const router = require('express').Router();
 
 router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
+  res.sendFile(path.join(__dirname, "../../public/index.html"));
 });
 
 router.get("/animals", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/animals.html"));
+  res.sendFile(path.join(__dirname, "../../public/animals.html"));
 });
 
 router.get("/zookeepers", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
+  res.sendFile(path.join(__dirname, "../../public/zookeepers.html"));
 });
 
 module.exports = router;
