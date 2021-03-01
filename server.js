28c5ef51f2fd4aa76875c9c7d6b6dd624b871d61
w.hunt <wfh2d88@gmail.com>
2021-03-01 10:08:03 -0600

    for ABC

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
-const express = require("express");
+const express = require('express');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
-// parse incoming string or array data
-app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
-app.use(express.json());
-app.use(express.static('public'));
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Use apiRoutes
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-const { animals } = require("./data/animals");
-const fs = require('fs');
-const path = require('path');
-
-
 app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}`);
+  console.log(`API server now on port ${PORT}!`);
 });
